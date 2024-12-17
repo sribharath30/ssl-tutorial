606cb4fc9f1274c3654883620b227f9dab7f1c28
sri.bharath.r.a@gmail.com <sri.bharath.r.a@gmail.com>
2024-12-17 23:21:24 +0530

    Establishing an SSL connection using Node.js

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from "express";
+import https from 'https';
+import fs from 'fs';
+
+
+const app = express();
+const PORT = '3000';
+const options = {
+    key: fs.readFileSync('.ssl/private-key.pem'),
+    cert: fs.readFileSync('.ssl/certificate.pem')
+  };
+app.get('/',(req,res)=>{
+    res.send('Hello world');
+});
+
+
+https.createServer(options,app).listen(PORT,()=>{
+    console.log(`Server running on port: ${PORT}`);
+})
